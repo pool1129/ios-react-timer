2cb894494687d4d67e47605c84c9dbfab312bc72
gayeong <pool1129@naver.com>
2024-09-09 17:29:14 +0900

    [feature] add dist files

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,1 @@
-export {};
+declare const TimePicker: any;
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,4 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const TimePicker_1 = __importDefault(require("./components/TimePicker"));
-module.exports = {
-    TimePicker: TimePicker_1.default,
-};
+const TimePicker = require("./components/TimePicker");
+module.exports = { TimePicker };
 //# sourceMappingURL=index.js.map
